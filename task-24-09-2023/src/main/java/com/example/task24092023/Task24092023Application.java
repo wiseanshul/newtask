47de04e8ec2c12ce10a557bdb5a3ae2876f9a3c2
wiseanshul <143176061+wiseanshul@users.noreply.github.com>
2023-12-01 15:26:33 +0530

    Update Task24092023Application.java inn github

--- task-24-09-2023/src/main/java/com/example/task24092023/Task24092023Application.java
+++ task-24-09-2023/src/main/java/com/example/task24092023/Task24092023Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Task24092023Application {
 
 	public static void main(String[] args) {
+		//this commant is added by anshul on github
 		SpringApplication.run(Task24092023Application.class, args);
 	}
 
