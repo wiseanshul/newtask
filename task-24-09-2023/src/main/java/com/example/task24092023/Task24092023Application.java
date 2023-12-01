2a646040d099aca536d26c198c3bff10e6a93829
wiseanshul <143176061+wiseanshul@users.noreply.github.com>
2023-12-01 15:33:35 +0530

    Update Task24092023Application.java

--- task-24-09-2023/src/main/java/com/example/task24092023/Task24092023Application.java
+++ task-24-09-2023/src/main/java/com/example/task24092023/Task24092023Application.java
@@ -9,6 +9,7 @@ public class Task24092023Application {
 	public static void main(String[] args) {
 		//this commant is added by anshul on github
 		SpringApplication.run(Task24092023Application.class, args);
+		System.out.println("hello world!");
 	}
 
 }
