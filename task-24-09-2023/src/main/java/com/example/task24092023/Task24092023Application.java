92e5acf8e3a58075c685d4dc8c761bfd9107986e
Dipti Tawari <dipti_tiwari@naxgrp.com>
2023-12-01 13:12:19 +0530

    initial commit

---
+++ task-24-09-2023/src/main/java/com/example/task24092023/Task24092023Application.java
@@ -0,0 +1,13 @@
+package com.example.task24092023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task24092023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task24092023Application.class, args);
+	}
+
+}
